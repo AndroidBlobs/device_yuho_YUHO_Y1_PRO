07a6cd7e472489c376a0f015438c63902c54c764
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-27 03:10:47 +0000

    full_k39tv1_bsp_512-user 8.1.0 O11019 1530698598 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/yuho/YUHO_Y1_PRO
+BOARD_VENDOR := yuho
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/yuho/YUHO_Y1_PRO/BoardConfigVendor.mk
